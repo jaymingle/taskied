6e85b502f2009632c27a1996e238585f85f3fb38
Jay Mingle <jkawudi@gmail.com>
2024-03-21 15:27:56 +0000

    Change

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ function App() {
 
     const projectOwnerName = 'John'
     const projectOwnerAge = Math.floor(Math.random() * 100) + 1
+    const projectFamilies = ['John', 'Jane', 'Sergio', 'Roland', 'Sally']
 
   return (
     <>
         <p>The name of the project owner is {projectOwnerName}</p>
         <p>The Project Number is {projectOwnerAge} years old</p>
-        <h1>Hello World</h1>
+        <p>My family members are {projectFamilies}</p>
     </>
   )
 }
