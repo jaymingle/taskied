27588ea92cf928e171dea2a6c18027e4f0453304
Jay Mingle <jkawudi@gmail.com>
2024-03-21 15:14:43 +0000

    Change

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import './App.css'
 
 function App() {
 
+    const projectOwnerName = 'John'
+    const projectOwnerAge = Math.floor(Math.random() * 100) + 1
+
   return (
     <>
+        <p>The name of the project owner is {projectOwnerName}</p>
+        <p>The Project Number is {projectOwnerAge} years old</p>
         <h1>Hello World</h1>
     </>
   )
