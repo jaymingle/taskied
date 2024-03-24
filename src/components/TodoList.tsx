81066e26c7353b8d4dcaf57a8c295e09785444a8
Jay Mingle <jkawudi@gmail.com>
2024-03-24 15:25:29 +0000

    Change

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css'
+
+const TodoList: React.FC =  () => {
+    return (
+        <div className="todos">
+            TodoList
+        </div>
+    );
+};
+
+export default TodoList;
