44809c760b7a939e09f43437997c7fcbacecde5f
Jay Mingle <jkawudi@gmail.com>
2024-03-24 13:23:29 +0000

    Change

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ const App: React.FC = () => {
 
     const [todo, setTodo] = useState<string>('')
 
-    console.log(todo)
-
   return (
     <div className="App">
         <span className="heading">Taskied</span>
