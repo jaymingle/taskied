592415d3aa8fc7c4eea0558b22668f767b1211e8
Jay Mingle <jkawudi@gmail.com>
2024-03-24 12:47:12 +0000

    Change

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import "./styles.css"
 
 const InputField = () => {
     return (
         <div>
-            Input
+            <form action="" className="input">
+                <input type="text" placeholder="Enter a task" className="input_box"/>
+                <input type="submit" value="Go" className="input_submit"/>
+            </form>
         </div>
     );
 };
