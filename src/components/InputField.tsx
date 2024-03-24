83b4daeb3940185aa82a77caaab8d41d9b062cad
Jay Mingle <jkawudi@gmail.com>
2024-03-24 13:13:29 +0000

    Change

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import "./styles.css"
 
 const InputField = () => {
+
+    const formSubmitHandler = e => {
+        e.preventDefault();
+    }
+
     return (
         <div>
-            <form action="" className="input">
+            <form onSubmit={formSubmitHandler} className="input">
                 <input type="text" placeholder="Enter a task" className="input_box"/>
                 <input type="submit" value="Go" className="input_submit"/>
             </form>
