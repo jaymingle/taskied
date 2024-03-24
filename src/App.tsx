aa8164c91b44c2627e4873c518547f837d858a27
Jay Mingle <jkawudi@gmail.com>
2024-03-24 14:13:02 +0000

    Change

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,22 @@ const App: React.FC = () => {
     const [todo, setTodo] = useState<string>('')
     const [todos, setTodos] = useState<Todo[]>([])
 
-    const handleAdd = e => {
+    const handleAdd = (e: React.FormEvent<EventTarget>): void => {
+        e.preventDefault();
+        console.log(todo)
 
+        if(todo){
+            setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}])
+        }
+        console.log(todos)
+
+        setTodo('')
     }
 
   return (
     <div className="App">
         <span className="heading">Taskied</span>
-        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd()}/>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
   )
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -5,7 +5,7 @@ import "./styles.css"
 interface Props{
     todo: string,
     setTodo:  React.Dispatch<React.SetStateAction<string>>
-    handleAdd: () => void
+    handleAdd: (e: React.FormEvent<EventTarget>) => void
 }
 
 
