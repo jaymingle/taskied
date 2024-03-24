9174b04e31b3996c19866205575da9296bf7d344
Jay Mingle <jkawudi@gmail.com>
2024-03-24 14:00:05 +0000

    Change

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,16 @@ import {Todo} from "./model.ts";
 const App: React.FC = () => {
 
     const [todo, setTodo] = useState<string>('')
-    const [todos, setTodos] = useState<Todo>([])
+    const [todos, setTodos] = useState<Todo[]>([])
+
+    const handleAdd = e => {
+
+    }
+
   return (
     <div className="App">
         <span className="heading">Taskied</span>
-        <InputField todo={todo} setTodo={setTodo}/>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd()}/>
     </div>
   )
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -5,14 +5,15 @@ import "./styles.css"
 interface Props{
     todo: string,
     setTodo:  React.Dispatch<React.SetStateAction<string>>
+    handleAdd: () => void
 }
 
 
-const InputField: React.FC<Props> = ({todo, setTodo}) => {
+const InputField: React.FC<Props> = ({todo, setTodo, handleAdd}) => {
 
     return (
         <div>
-            <form className="input">
+            <form onSubmit={handleAdd} className="input">
                 <input type="text" value={todo} onChange={e => setTodo(e.target.value)} placeholder="Enter a task" className="input_box"/>
                 <input type="submit" value="Go" className="input_submit"/>
             </form>
