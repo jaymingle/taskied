6bd7347c387c4a47ac3c708371414c5d630d9068
Jay Mingle <jkawudi@gmail.com>
2024-03-24 13:56:37 +0000

    Change

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
 import React, {useState} from "react";
 import InputField from "./components/InputField.tsx";
+import {Todo} from "./model.ts";
 
 const App: React.FC = () => {
 
     const [todo, setTodo] = useState<string>('')
-
+    const [todos, setTodos] = useState<Todo>([])
   return (
     <div className="App">
         <span className="heading">Taskied</span>
