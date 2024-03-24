a9376e06465765a86f2ff0fa1e9605589c734205
Jay Mingle <jkawudi@gmail.com>
2024-03-24 01:11:20 +0000

    Change

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,6 @@ import './App.css'
 
 function App() {
 
-    const projectOwnerName = 'John'
-    const projectOwnerAge = Math.floor(Math.random() * 100) + 1
-    const projectFamilies = ['John', 'Jane', 'Sergio', 'Roland', 'Sally']
-
-    type Employee = {
-        name?: string,
-        age: number
-    }
-
-    const employeeOne: Employee = {
-        age: 45,
-        name: 'Kwame Nsoh'
-    }
-
-    function showName(name: string){
-        console.log(name)
-    }
-
-    showName('Jay Mingle')
 
   return (
     <>
