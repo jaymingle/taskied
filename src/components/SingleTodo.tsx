a4da642d48a690fe70f7073f236c84290e74f254
Jay Mingle <jkawudi@gmail.com>
2024-03-25 10:13:15 +0000

    Change

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -7,17 +7,25 @@ type Props = {
     todo: Todo,
     todos: Todo[],
     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
-
 }
 
 const SingleTodo = ({todo, todos, setTodos}:Props) => {
+
+    const handleDone = (id: number) => {
+        // console.log(id)
+        setTodos(
+            todos.map(todo => todo.id === id ? {...todo, isDone: !todo.isDone} : todo)
+        )
+
+    }
+
     return (
         <form className="todos_single">
             <span className="todos_single--text">{todo.todo}</span>
             <div>
                 <span className="icon"><AiFillEdit/></span>
                 <span className="icon"><AiFillDelete/></span>
-                <span className="icon"><AiFillCheckSquare/></span>
+                <span className="icon" onClick={() => handleDone(todo.id)}><AiFillCheckSquare/></span>
             </div>
         </form>
     );
